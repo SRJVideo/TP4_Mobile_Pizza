605dac92dde65bc885dde5f984ff9ec7beb20f3c
MabJo <1931631@cegeplimoilou.ca>
2022-11-29 20:13:14 -0500

    Ajout d'un fragment
    Pour afficher un fragment

--- app/src/main/java/com/example/tp4_pizzaapplication/activity/MainActivity.java
+++ app/src/main/java/com/example/tp4_pizzaapplication/activity/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.tp4_pizzaapplication.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.view.MenuItem;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.regex.Matcher;
@@ -29,6 +31,11 @@ public class MainActivity extends AppCompatActivity {
         return sharedPreferences.getBoolean("login",false);
     }
 
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     /**
      * Redirige vers login page
