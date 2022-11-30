1ce17839c0cb461b0efe9125f303d658b1b5d2f7
MabJo <1931631@cegeplimoilou.ca>
2022-11-29 19:13:20 -0500

    Ajout Accueil Activity
    ecran d'accueil

--- app/src/main/java/com/example/tp4_pizzaapplication/MainActivity.java
+++ app/src/main/java/com/example/tp4_pizzaapplication/MainActivity.java
@@ -1,14 +1,77 @@
 package com.example.tp4_pizzaapplication;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.view.MenuItem;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+;
+
+/**
+ * cette classe sera utilisée comme activité parente pour toutes les autres activités de l'application
+ */
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    Context context;
+    Intent intent;
+    SharedPreferences sharedPreferences;
+    String SHARED_PREF_NAME ="user_pref";
+    SharedPreferences.Editor sharedPrefEditor;
+    protected String name, prenom, email,password,pays;
+
+
+    protected boolean isLoggedIn(){
+        return sharedPreferences.getBoolean("login",false);
     }
+
+    /**
+     * Redirige vers login page
+     */
+    protected void logout(){
+        sharedPrefEditor.putBoolean("login",false);
+        sharedPrefEditor.apply();
+        sharedPrefEditor.commit();
+    }
+
+    /**
+     * permet la validation de l'email
+     * @param email
+     * @return true ou false si le courriel est valide
+     */
+    public static boolean isEmailValid(String email) {
+        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    /**
+     * permet d'initialisé les préférences
+     */
+    public void init() {
+        sharedPreferences = getSharedPreferences(SHARED_PREF_NAME,MODE_PRIVATE);
+        sharedPrefEditor = sharedPreferences.edit();
+    }
+
+    /**
+     * Fait une action particulière si on clique si le menu
+     * @param item
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+  
 }
