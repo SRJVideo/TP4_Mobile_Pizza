340d818c14193e6eb1967d30a240135eacff202e
MabJo <1931631@cegeplimoilou.ca>
2022-12-06 22:55:13 -0500

    sgeges

--- UtileTp4/app/src/main/java/com/example/utiletp4/MainActivity.java
+++ UtileTp4/app/src/main/java/com/example/utiletp4/MainActivity.java
@@ -12,6 +12,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 
+import com.example.utiletp4.fragment.PizzaFragment;
 import com.example.utiletp4.modele.User;
 import com.example.utiletp4.ui.InscriptionFragment;
 import com.example.utiletp4.ui.home.HomeFragment;
@@ -62,9 +63,14 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(this, "Nom User:"+currentUser.getName()+" Email:"+currentUser.getEmail(), Toast.LENGTH_SHORT).show();
                     break;
 
-                case R.id.profile:
+                case R.id.nav_profile:
                     frag = new InscriptionFragment();
                     break;
+
+                case R.id.nav_pizzas:
+                    frag = new PizzaFragment();
+                    break;
+
             }
 
             Toast.makeText(getApplicationContext(), menuItem.getTitle(), Toast.LENGTH_SHORT).show();
--- UtileTp4/app/src/main/java/com/example/utiletp4/fragment/PizzaFragment.java
+++ UtileTp4/app/src/main/java/com/example/utiletp4/fragment/PizzaFragment.java
@@ -1,27 +1,60 @@
 package com.example.utiletp4.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.utiletp4.MainActivity;
 import com.example.utiletp4.R;
+import com.example.utiletp4.database.DatabaseManager;
+import com.example.utiletp4.modele.Pizza;
+import com.example.utiletp4.ui.adapter.PizzaListAdapter;
+
+import java.util.List;
 
 public class PizzaFragment extends Fragment {
 
+    private MainActivity mainActivity;
+    DatabaseManager dbm;
 
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_pizza, container, false);
-
-    }
-
+    @Nullable
     @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_pizza, container, false);
+        // Instantiate object DB and View
+        dbm = new DatabaseManager(getActivity());
+        afficherlesPizzas(root);
+        return root;
     }
+
+    // Called when a fragment is first attached to its context.
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+
+        if (context instanceof MainActivity) {
+            this.mainActivity = (MainActivity) context;
+        }
+    }
+
+    /**
+     * afficher les pizzas listPizzas
+     */
+    private void afficherlesPizzas(View v){
+        ListView listPizzas = v.findViewById(R.id.listViewPizzas);
+
+        PizzaListAdapter adapter = new PizzaListAdapter(v.getContext(), dbm.readPizza());
+        listPizzas.setAdapter(adapter);
+    }
+    //Mes liens d'aides (Sam)
+    //https://www.youtube.com/watch?v=PoPJ-OoBONM
+    //https://www.youtube.com/watch?v=_ad6HI94dFQ
 }
