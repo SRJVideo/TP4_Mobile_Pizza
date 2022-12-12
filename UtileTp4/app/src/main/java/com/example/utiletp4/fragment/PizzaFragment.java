767265544e73e8f68f9ba72c1febb2e944de6ef0
MabJo <1931631@cegeplimoilou.ca>
2022-12-12 17:15:20 -0500

    modification PizzaFragment

--- UtileTp4/app/src/main/java/com/example/utiletp4/fragment/PizzaFragment.java
+++ UtileTp4/app/src/main/java/com/example/utiletp4/fragment/PizzaFragment.java
@@ -30,43 +30,6 @@ import com.example.utiletp4.ui.adapter.PizzaListAdapter;
 import java.util.List;
 
 public class PizzaFragment extends Fragment {
-    DatabaseManager databaseManager;
-
-    void AjoutPizzaBD(){
-        //Sorte, bacon, cheese, pepperoni, garnie
-        Pizza pizza1 = new Pizza("Bacon", "petite", 6d);
-        Pizza pizza2 = new Pizza("Bacon", "moyenne", 10d);
-        Pizza pizza3 = new Pizza("Bacon", "grande", 12d);
-        Pizza pizza4 = new Pizza("Cheese", "petite", 8d);
-        Pizza pizza5 = new Pizza("Cheese", "moyenne", 11.5d);
-        Pizza pizza6 = new Pizza("Cheese", "grande", 14d);
-        Pizza pizza7 = new Pizza("Pepperoni", "petite", 10d);
-        Pizza pizza8 = new Pizza("Pepperoni", "moyenne", 13d);
-        Pizza pizza9 = new Pizza("Pepperoni", "grande", 16.5d);
-        Pizza pizza10 = new Pizza("Garnie", "petite", 14d);
-        Pizza pizza11 = new Pizza("Garnie", "moyenne", 18d);
-        Pizza pizza12 = new Pizza("Garnie", "grande", 22d);
-
-        if (databaseManager.readPizza().isEmpty()){
-            databaseManager.insertPizza(pizza1);
-            databaseManager.insertPizza(pizza2);
-            databaseManager.insertPizza(pizza3);
-            databaseManager.insertPizza(pizza4);
-            databaseManager.insertPizza(pizza5);
-            databaseManager.insertPizza(pizza6);
-            databaseManager.insertPizza(pizza7);
-            databaseManager.insertPizza(pizza8);
-            databaseManager.insertPizza(pizza9);
-            databaseManager.insertPizza(pizza10);
-            databaseManager.insertPizza(pizza11);
-            databaseManager.insertPizza(pizza12);
-        }
-
-        System.out.println(databaseManager.readPizza());
-
-
-    }
-
     private MainActivity mainActivity;
     DatabaseManager dbm;
 
@@ -76,8 +39,8 @@ public class PizzaFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_pizza, container, false);
         // Instantiate object DB and View
         dbm = new DatabaseManager(getActivity());
-
-        afficherlesPizzas(root);     AjoutPizzaBD();
+        if (dbm.readPizza().isEmpty()) AjoutPizzaBD();
+        afficherlesPizzas(root);
         return root;
     }
 
@@ -90,6 +53,25 @@ public class PizzaFragment extends Fragment {
             this.mainActivity = (MainActivity) context;
         }
     }
+
+
+    void AjoutPizzaBD(){
+        //Sorte, bacon, cheese, pepperoni, garnie
+        String[] sortes = {"Fromage","Péppéroni","Bacon","Garnie","Tomates","Végétarienne","Royale"};
+        String[] types = {"Petite","Moyenne","Grande"};
+
+            for (int i=0 ; i < sortes.length ; i++) {
+                double prix = (Math.random()*10);
+
+                for (int j=0 ; i <types.length; i++ ) {
+                    Pizza pizza = new Pizza(sortes[i], types[j], prix+j);
+
+                    dbm.insertPizza(pizza);
+                }
+            }
+    }
+
+
 
     /**
      * afficher les pizzas listPizzas
