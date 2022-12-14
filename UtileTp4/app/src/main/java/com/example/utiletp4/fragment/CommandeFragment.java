c87ca98f41458b5204a40f46cb9639f2d39ba1f8
MabJo <1931631@cegeplimoilou.ca>
2022-12-14 11:58:16 -0500

    Update CommandeFragment.java

--- UtileTp4/app/src/main/java/com/example/utiletp4/fragment/CommandeFragment.java
+++ UtileTp4/app/src/main/java/com/example/utiletp4/fragment/CommandeFragment.java
@@ -1,16 +1,19 @@
 package com.example.utiletp4.fragment;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 import com.example.utiletp4.MainActivity;
@@ -24,6 +27,8 @@ public class CommandeFragment extends Fragment {
 
     listCommandeArrayAdapter adapter;
     TextView textTotalCommande;
+    TextView textEconomie;
+    TextView textAvecPoints;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_commande, container, false);
@@ -40,7 +45,13 @@ public class CommandeFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         listView = view.findViewById(R.id.listpizza);
         textTotalCommande = view.findViewById(R.id.totalCommandeTextView);
+        textEconomie = view.findViewById(R.id.textViewEconomie);
+        textAvecPoints = view.findViewById(R.id.textViewTotalAvecPoints);
         listView.setAdapter(adapter);
+
+
+        Button btAddToCart = view.findViewById(R.id.button);
+        btAddToCart.setOnClickListener( new AfficherDialogue());
     }
 
     // Called when a fragment is first attached to its context.
@@ -53,11 +64,31 @@ public class CommandeFragment extends Fragment {
         }
     }
 
-
-
     public TextView getTextTotalCommande() {
         return textTotalCommande;
     }
 
 
+    private class AfficherDialogue implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            AlertDialog dialog = new AlertDialog.Builder(getContext()).create();
+
+            dialog.setTitle("ADD TO CART");
+            dialog.setMessage("Passez à la caisse pour paiement par Carte ?");
+            dialog.setButton(AlertDialog.BUTTON_POSITIVE, "OUI", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    PointsFragment points = new PointsFragment();
+                }
+            });
+            dialog.setButton(AlertDialog.BUTTON_NEGATIVE, "NON", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+
+                }
+            });
+            dialog.show();
+        }
+    }
 }
